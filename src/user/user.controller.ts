3d5c182be740a8945d39c76b9cc48e8365348e52
gryzun33 <yakushevao777@gmail.com>
2024-11-05 10:50:13 +0300

    feat: add updatepassword and deleteuser methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/createUser.dto';
 import { User, UserResponse } from './interfaces/user.interface';
+import { UpdatePasswordDto } from './dto/updatePassword.dto';
 
-@Controller('users')
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -20,5 +29,23 @@ export class UserController {
   @Post()
   create(@Body() createUserDto: CreateUserDto): UserResponse {
     return this.userService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ): UserResponse | undefined {
+    return this.userService.updatePassword(id, updatePasswordDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string): string {
+    const result = this.userService.deleteUser(id);
+    if (result) {
+      return `User with id ${id} has been deleted successfully.`;
+    } else {
+      return `User with id ${id} not found.`;
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { User, UserResponse } from './interfaces/user.interface';
 import { CreateUserDto } from './dto/createUser.dto';
 import { v4 as uuidv4 } from 'uuid';
+import { UpdatePasswordDto } from './dto/updatePassword.dto';
 
 @Injectable()
 export class UserService {
@@ -36,5 +37,34 @@ export class UserService {
       return userWithoutPassword;
     }
     return undefined;
+  }
+
+  updatePassword(
+    id: string,
+    updatePasswordDto: UpdatePasswordDto,
+  ): UserResponse | undefined {
+    const user = this.users.find((user) => user.id === id);
+    if (user) {
+      if (user.password !== updatePasswordDto.oldPassword) {
+        throw new Error('Old password is incorrect');
+      }
+
+      user.password = updatePasswordDto.newPassword;
+      user.updatedAt = Date.now();
+      user.version += 1;
+
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    }
+    return undefined;
+  }
+
+  deleteUser(id: string): boolean {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      this.users.splice(userIndex, 1);
+      return true;
+    }
+    return false;
   }
 }
