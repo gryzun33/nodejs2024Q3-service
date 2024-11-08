ed0245bf59e1d29eb0cad04905ecdf75aeba5daa
gryzun33 <yakushevao777@gmail.com>
2024-11-09 00:00:27 +0300

    feat: add artist doc

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -14,47 +14,82 @@ import {
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Artist } from './entities/artist.entity';
 
+@ApiTags('Artists')
 @Controller('artist')
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
   @ApiOperation({ summary: 'Create new artist' })
-  create(@Body() createArtistDto: CreateArtistDto) {
+  @ApiResponse({
+    status: 201,
+    description: 'The artist has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request. Invalid input.' })
+  create(@Body() createArtistDto: CreateArtistDto): Artist {
     return this.artistService.create(createArtistDto);
   }
 
   @Get()
   @ApiOperation({ summary: 'Get all artists' })
-  findAll() {
+  @ApiResponse({
+    status: 200,
+    description: 'Successful operation.',
+    type: [Artist],
+  })
+  findAll(): Artist[] {
     return this.artistService.findAll();
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Get single artist by id' })
-  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+  @ApiResponse({
+    status: 200,
+    description: 'Successful operation.',
+    type: Artist,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request. Invalid UUID.' })
+  @ApiResponse({ status: 404, description: 'Artist not found.' })
+  findOne(@Param('id', new ParseUUIDPipe()) id: string): Artist {
     const artist = this.artistService.findOne(id);
     if (!artist) {
-      throw new NotFoundException(`Artist not found.`);
+      throw new NotFoundException('Artist not found.');
     }
     return artist;
   }
 
   @Put(':id')
   @ApiOperation({ summary: 'Update artist info' })
+  @ApiResponse({
+    status: 200,
+    description: 'The artist has been successfully updated.',
+    type: Artist,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. Invalid UUID or input.',
+  })
+  @ApiResponse({ status: 404, description: 'Artist not found.' })
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateArtistDto: UpdateArtistDto,
-  ) {
+  ): Artist {
     return this.artistService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Delete artist' })
-  remove(@Param('id', new ParseUUIDPipe()) id: string) {
+  @ApiResponse({
+    status: 204,
+    description: 'The artist has been successfully deleted.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request. Invalid UUID.' })
+  @ApiResponse({ status: 404, description: 'Artist not found.' })
+  remove(@Param('id', new ParseUUIDPipe()) id: string): void {
     return this.artistService.remove(id);
   }
 }
--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Artist {
+  @ApiProperty({
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    description: 'Unique identifier for the artist',
+  })
   id: string;
+  @ApiProperty({ example: 'Billie Eilish ', description: 'The name of artist' })
   name: string;
+  @ApiProperty({
+    example: true,
+    description: 'Indicates whether the artist has won a Grammy award.',
+  })
   grammy: boolean;
 }
